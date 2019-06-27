60b6e083a9b5239991961d18f1bbf0b23d8aa66f
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-06-27 03:57:47 -0300

    Better descriptions; renamed files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bootstrap VPS Shellscript
+**[Public Draft] Simple portable shell script bootstrap VPSs. Focused to help
+deal with very, very cheap infrastructure providers and when using Chef, Puppet,
+Ansible, Saltstack can be overkill**.
+
+## Features
+- Add public keys
+- (...)
+
--- bootstrap-vps.sh
+++ bootstrap-vps.sh
@@ -20,14 +20,55 @@
 #===============================================================================
 
 function usage {
-    echo "usage: VPS_PASS=PassWords `basename $0` -f"
-    echo "  -f      force run script (you agree with security notices)"
+    echo "`basename $0` usage: "
+    echo "With PARAMETERS:"
+    echo ""
+    echo "usage: `basename $0` -p ThePassWord -u root -h example.com -f all"
+    echo "  -f      [REQUIRED] force run script (you agree with security notices"
+    echo "  -h      [optional] user@host"
+    echo "  -k      [optional] specify a public key to use"
+    echo ""
+    echo "With ENVIROMENT VARIABLES"
+    echo "   PASSWORD=PassWords HOST=host `basename $0` -f"
+    echo "With FILES (Recomended!)"
+    echo ""
+    echo " (not implemented yet)"
     echo ""
     echo "Security notes:"
+    echo ""
     echo " 1. This script have similar security as the use of sshpass."
     echo " 2. This script (when forcing remove know_hosts) is not recommented."
+    echo ""
     exit 1
 }
+
+while getopts "fhkup:" opt; do
+  case $opt in
+    f)
+      echo "-f was triggered, Parameter: $OPTARG" >&2
+      ;;
+    h)
+      echo "-h was triggered, Parameter: $OPTARG" >&2
+      ;;
+    k)
+      echo "-k was triggered, Parameter: $OPTARG" >&2
+      ;;
+    p)
+      echo "-p was triggered, Parameter: $OPTARG" >&2
+      ;;
+    p)
+      echo "-u was triggered, Parameter: $OPTARG" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    #:)
+    #  echo "Option -$OPTARG requires an argument." >&2
+    #  exit 1
+    #  ;;
+  esac
+done
 
 echo "VPS_PASS: $BVPS_PASS"
 
